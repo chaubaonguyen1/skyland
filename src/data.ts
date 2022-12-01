c7e98dab92a8ebdd9af220db062b09887b5f960d
Chau Nguyen <matthewdns@gmail.com>
2022-12-01 10:28:05 +0700

    Add files

--- src/data.ts
+++ src/data.ts
@@ -8,14 +8,14 @@ export const SliderItems = [
   {
     id: 1,
     img: baidai1,
-    title: "DỰ ÁN ĐẤT NỀN BÃI BÃI DÀI",
+    title: "ĐẤT NỀN BÃI BÃI DÀI",
     link: "dat-nen-bai-dai",
-    introduction: `Phát triển cùng với khu Đô thị sinh thái và đón sóng Siêu dự án Đô thị Đại học Quốc Gia Hà Nội cơ sở tại Hòa Lạc. Đất nền Bãi Dài trở thành nước cờ đầu tư sinh lời số 1 tại thị trường Hòa Lạc được các nhà đầu tư săn đón.`,
+    introduction: `Phát triển cùng với khu Đô thị sinh thái và đón sóng Siêu Đô thị Đại học Quốc Gia Hà Nội cơ sở tại Hòa Lạc. Đất nền Bãi Dài trở thành nước cờ đầu tư sinh lời số 1 tại thị trường Hòa Lạc được các nhà đầu tư săn đón.`,
   },
   {
     id: 2,
     img: phuctien1,
-    title: `DỰ ÁN SIÊU MỎ VÀNG PHÚC TIẾN `,
+    title: `SIÊU MỎ VÀNG PHÚC TIẾN `,
     link: "dat-nen-phuc-tien",
     introduction: `Sở hữu vị trí đặc biệt hiếm hoi tại vùng phát triển nhất xã Phúc Tiến.
     Nằm tại "tọa độ vàng" trung tâm ngã Tư, là nơi giao thoa 2 trục đường huyết mạch quan trọng là Tỉnh lộ 420 và trục Hồ Tây - Ba Vì.
@@ -24,14 +24,14 @@ export const SliderItems = [
   {
     id: 3,
     img: codong1,
-    title: `DỰ ÁN ĐẤT NỀN CỔ ĐÔNG`,
+    title: `ĐẤT NỀN CỔ ĐÔNG`,
     link: "dat-nen-co-dong",
     introduction: `Với vị trí đắc địa nằm trên mặt đường trục chính, phát triển mạnh mẽ cùng với khu du lịch nghỉ dưỡng tại Sơn Tây, Đất nền Cổ Đông trở thành 1 trong những lựa chọn hàng đầu của các nhà đầu tư sáng giá!.`,
   },
   {
     id: 4,
     img: yenmy3,
-    title: `DỰ ÁN ĐẤT NỀN YÊN MỸ`,
+    title: `ĐẤT NỀN YÊN MỸ`,
     link: "dat-nen-yen-my",
     introduction: `Là tâm điểm kết nối hoàn hảo thúc đẩy sự phát triển giữa khu Công Nghệ Cao Hòa Lạc và các thị trấn liên quan, gắn kết Đô thị trung tâm với Đô thị vệ tinh Hòa Lạc.
     `,
@@ -39,14 +39,14 @@ export const SliderItems = [
   {
     id: 5,
     img: phutho1,
-    title: `DỰ ÁN ĐẤT NỀN PHÚ THỌ`,
+    title: `ĐẤT NỀN PHÚ THỌ`,
     link: "dat-nen-phu-tho",
     introduction: `Nắm bắt được sức nóng của thị trường bất động sản và tiềm năng phát triển mạnh mẽ khi nằm giữa 2 khu Công Nghiệp Tam Nông và khu Công Nghiệp Trung Hà – Phú Thọ, SKY LAND mang đến cho quý anh chị nhà đầu tư quỹ đất vô cùng tiềm năng: “Đất nền Phú Thọ”.`,
   },
   {
     id: 6,
     img: tanxa1,
-    title: `DỰ ÁN ĐẤT NỀN TÂN XÃ`,
+    title: `ĐẤT NỀN TÂN XÃ`,
     link: "dat-nen-tan-xa",
     introduction: `Sở hữu vị trí kim cương sát vách với khu Công Nghệ Cao Hòa Lạc, phân khu Tân Xã phát triển song song cùng với sự phát triển của Thành phố Khoa học Công Nghệ Cao. Đất nền Tân Xã, mảnh ghép đầu tư siêu giá trị.`,
   },
